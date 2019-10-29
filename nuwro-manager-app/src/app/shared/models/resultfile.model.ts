53879730348cd794c6c3ac190856997e0670924c
CukierPuder <cukier_puder@hotmail.com>
2019-10-29 10:33:42 +0100

    [#28g1u5] Generate Resultfile model

---
+++ nuwro-manager-app/src/app/shared/models/resultfile.model.ts
@@ -0,0 +1,34 @@
+import { Experiment } from './experiment.model';
+import { Measurement } from './measurement.model';
+import { Nuwroversion } from './nuwroversion.model';
+import { Datafile } from './datafile.model';
+
+export class Resultfile {
+  constructor(
+    public experiment: Experiment,
+    public measurement: Measurement,
+    public nuwroversion: Nuwroversion,
+    public description: string,
+    public x_axis: string,
+    public y_axis: string,
+    public filename: string,
+    public result_file: File,
+    public related_datafiles: Array<Datafile>
+    public link?: string,
+    public creation_date?: Date,
+    public id?: number
+  ) { }
+
+  public toString(): string {
+    return this.filename;
+  }
+
+  public getArrayOfDatafileIds(): Array<number> {
+    const ids: Array<number>;
+    for (let datafile of this.related_datafiles) {
+      ids.push(datafile.id);
+    }
+
+    return ids;
+  }
+}
